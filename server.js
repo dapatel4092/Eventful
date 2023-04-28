bd81cffd41fb007218986459da4b255aae6d85f4
Dhruv <dapatel700@gmail.com>
2023-04-27 21:07:04 -0700

    added depdencies and set up basics ( express app, handlebar engine, port server listening to server.js

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
+// Dependencies
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const hbs = exphbs.create({});
+
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/homeroutes'));
+
+// Starts the server to begin listening
+app.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:' + PORT);
+});
+
+
 //API for event brite
 // const eventbriteUrl = "https://www.eventbriteapi.com/v3/users/me/?token="
 // const eventbriteKey = "LMZTLLTII2KJCUS7PBHH"
