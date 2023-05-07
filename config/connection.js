3dd2c875a28512f54ef3086ff2e14b887c4349f4
Dhruv <dapatel700@gmail.com>
2023-05-06 18:52:06 -0700

    got connections working and fixed some issues with handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,25 @@
+require('dotenv').config();
 const Sequelize = require('sequelize');
 
 
+// const sequelize = new Sequelize(
+//   process.DB_NAME,
+//   process.DB_USER,
+//   process.DB_PASSWORD,
+//   {
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     port: 3306
+//   }
+// );
 const sequelize = new Sequelize(
-  process.DB_NAME,
-  process.DB_USER,
-  process.DB_PASSWORD,
+"Eventful_db",
+  "root",
+  "JackandJillwentupthehill!",
   {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3001
+    port: 3306
   }
 );
 
